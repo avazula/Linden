79c24a66f46831f1b0ddf6e7958c58e7e0230bf2
Marie Brunet <marie.brunet@insa-rennes.fr>
2017-03-29 10:19:46 +0200

    Creation of the class Plant

---
+++ Linden/Plant.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stream>
+
+class Plant {
+private:
+	string name;
+	bool indoors[12];
+	bool outdoors[12];
+	bool harvest[12];
+
+	Plant();
+	~Plant();
+
+public:
+
+};
